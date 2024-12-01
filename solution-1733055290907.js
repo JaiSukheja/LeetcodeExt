7d1ddf8407d2b53d0a0e7f9876b7c29cda12028c
Jai Sukheja <97387255+JaiSukheja@users.noreply.github.com>
2024-12-01 17:44:55 +0530

    Add LeetCode solution

---
+++ solution-1733055290907.js
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int s=i+1,e=n-1;
+            if(arr[i]<0){
+                while(s<=e){
+                    int m=s+(e-s)/2;
+                    if(arr[m]==arr[i]/2.0){
+                        return 1;
+                    }
+                    else if(arr[m]<arr[i]/2.0){
+                        s=m+1;
+                    }
+                    else{
+                        e=m-1;
+                    }
+                }
+            }
+            else{
+                while(s<=e){
+                    int m=s+(e-s)/2;
+                    if(arr[m]==arr[i]*2){
+                        
+                        return 1;
+                    }
+                    else if(arr[m]<arr[i]*2){
