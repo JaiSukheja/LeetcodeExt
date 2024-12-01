c295878979e8a463578645f471f34f2032f296b1
Jai Sukheja <97387255+JaiSukheja@users.noreply.github.com>
2024-12-02 01:18:22 +0530

    Add LeetCode solution

--- median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays.cpp
@@ -1,4 +1,4 @@
-Can you solve this real interview question? Median of Two Sorted Arrays - Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+/*Can you solve this real interview question? Median of Two Sorted Arrays - Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
 The overall run time complexity should be O(log (m+n)).
 
@@ -29,7 +29,7 @@ Constraints:
  * 0 <= m <= 1000
  * 0 <= n <= 1000
  * 1 <= m + n <= 2000
- * -106 <= nums1[i], nums2[i] <= 106
+ * -106 <= nums1[i], nums2[i] <= 106*/
 
 
 class Solution {
