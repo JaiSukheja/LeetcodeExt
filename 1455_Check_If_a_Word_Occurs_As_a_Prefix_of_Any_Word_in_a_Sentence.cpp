2ce42889c44cd11929e5972893bdc8c97d778d7b
Jai Sukheja <97387255+JaiSukheja@users.noreply.github.com>
2024-12-03 18:43:06 +0530

    Add LeetCode solution

--- 1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp
+++ 1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp
@@ -1,43 +1,49 @@
-/*Can you solve this real interview question? Check If a Word Occurs As a Prefix of Any Word in a Sentence - Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+/*Can you solve this real interview question? Adding Spaces to a String - You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. Each space should be inserted before the character at the given index.
 
-Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+ * For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
 
-A prefix of a string s is any leading contiguous substring of s.
+Return the modified string after the spaces have been added.
 
  
 
 Example 1:
 
 
-Input: sentence = "i love eating burger", searchWord = "burg"
-Output: 4
-Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+Output: "Leetcode Helps Me Learn"
+Explanation: 
+The indices 8, 13, and 15 correspond to the underlined characters in "LeetcodeHelpsMeLearn".
+We then place spaces before those characters.
 
 
 Example 2:
 
 
-Input: sentence = "this problem is an easy problem", searchWord = "pro"
-Output: 2
-Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
+Input: s = "icodeinpython", spaces = [1,5,7,9]
+Output: "i code in py thon"
+Explanation:
+The indices 1, 5, 7, and 9 correspond to the underlined characters in "icodeinpython".
+We then place spaces before those characters.
 
 
 Example 3:
 
 
-Input: sentence = "i am tired", searchWord = "you"
-Output: -1
-Explanation: "you" is not a prefix of any word in the sentence.
+Input: s = "spacing", spaces = [0,1,2,3,4,5,6]
+Output: " s p a c i n g"
+Explanation:
+We are also able to place spaces before the first character of the string.
 
 
  
 
 Constraints:
 
- * 1 <= sentence.length <= 100
- * 1 <= searchWord.length <= 10
- * sentence consists of lowercase English letters and spaces.
- * searchWord consists of lowercase English letters.*/
+ * 1 <= s.length <= 3 * 105
+ * s consists only of lowercase and uppercase English letters.
+ * 1 <= spaces.length <= 3 * 105
+ * 0 <= spaces[i] <= s.length - 1
+ * All the values of spaces are strictly increasing.*/
 
 
 class Solution {
