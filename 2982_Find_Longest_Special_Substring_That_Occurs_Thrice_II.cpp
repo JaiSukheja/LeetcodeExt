768d3c8c64a1b2299ff4714230e2c6c860e3ce14
Jai Sukheja <97387255+JaiSukheja@users.noreply.github.com>
2024-12-10 14:00:06 +0530

    Add LeetCode solution

---
+++ 2982_Find_Longest_Special_Substring_That_Occurs_Thrice_II.cpp
@@ -0,0 +1,76 @@
+/*Can you solve this real interview question? Find Longest Special Substring That Occurs Thrice II - You are given a string s that consists of lowercase English letters.
+
+A string is called special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
+
+Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring occurs at least thrice.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+ 
+
+Example 1:
+
+
+Input: s = "aaaa"
+Output: 2
+Explanation: The longest special substring which occurs thrice is "aa": substrings "aaaa", "aaaa", and "aaaa".
+It can be shown that the maximum length achievable is 2.
+
+
+Example 2:
+
+
+Input: s = "abcdef"
+Output: -1
+Explanation: There exists no special substring which occurs at least thrice. Hence return -1.
+
+
+Example 3:
+
+
+Input: s = "abcaba"
+Output: 1
+Explanation: The longest special substring which occurs thrice is "a": substrings "abcaba", "abcaba", and "abcaba".
+It can be shown that the maximum length achievable is 1.
+
+
+ 
+
+Constraints:
+
+ * 3 <= s.length <= 5 * 105
+ * s consists of only lowercase English letters.*/
+
+
+class Solution {
+public:
+    int maximumLength(string s) {
+        int n = s.length();
+        vector<vector<int>> matrix(26,vector<int>(n+1,0));
+        char prev_char = s[0];
+        int length = 0;
+        for(int i=0;i<n;i++){
+            char curr_char = s[i];
+            if(curr_char == prev_char){
+                length += 1;
+                matrix[curr_char-'a'][length] += 1;
+            }else{
+                length = 1;
+                matrix[curr_char-'a'][length] += 1;
+                prev_char = curr_char;
+            }
+        }
+        int result = 0;
+        for(int i=0;i<26;i++){
+            int cumSum = 0;
+            for(int j = n;j>=1;j--){
+                cumSum += matrix[i][j];
+                if(cumSum >= 3){
+                    result = max(result,j);
+                    break;
+                }
+            }
+        }
+        return result == 0 ? -1 : result;
+    }
+};
